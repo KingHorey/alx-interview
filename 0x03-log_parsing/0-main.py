258fba11e93325e135f9d6d5e8799217e418f0b4
kingHorey <Oreoluwaakinbo.oa@gmail.com>
2024-06-24 13:26:48 +0100

    Modified logic
    Added sys.stdin.readlines() to check the number of args passed to stdin
    as sys.argv did not suffice
    Modified logic to cater for when certain codes are right but whitespaces
    are not properly formatted

--- 0x03-log_parsing/0-main.py
+++ 0x03-log_parsing/0-main.py
@@ -3,14 +3,22 @@ import random
 import sys
 from time import sleep
 import datetime
+import fileinput
 
-for i in range(10000):
-    sleep(random.random())
-    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
-        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
-        datetime.datetime.now(),
-        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-        random.randint(1, 1024)
-    ))
-    sys.stdout.flush()
+with fileinput.input(files=('inputs.txt')) as f_input:
+    for line in f_input:
+        sleep(random.random())
+        sys.stdout.write(line)
+        sys.stdout.flush()
+
+# for i in range(10000):
+#     sleep(random.random())
+#     sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
+#         random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
+#         datetime.datetime.now(),
+#         random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+#         random.randint(1, 1024)
+#     ))
+#     sys.stdout.flush()
+
 
--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,31 +4,76 @@
 
 import sys
 import re
+import signal
 
 sys_inputs = sys.stdin
 lines_count = 0
+rounds = 0
 file_size = 0
 status_codes = {}
-for sys_input in sys_inputs:
-    sys_input = sys_input.strip()
+converted = False
+converted_code = False
+valid_codes = [200, 301, 400, 401, 403, 404, 405, 500]
+valid_code = False
+
+
+def print_result(codes=None) -> None:
+    print("File size: {}".format(file_size))
+    if codes:
+        for key, value in sorted(codes.items()):
+            print("{}: {}".format(key, value))
+
+
+def signal_handler(sign, frame):
+    print_result(status_codes)
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, signal_handler)
+
+input_list = sys_inputs.readlines()
+input_size = len(input_list)
+
+if input_size == 0:
+    print_result()
+
+for i in range(input_size):
+    sys_input = input_list[i].strip()
     lines_count += 1
+    rounds += 1
     """ check with regex if the input matches pattern """
-    regex_check = re.match(r'(.*?) - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{'
+    regex_check = re.match(r'(.*?).?-.?\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{'
                            r'2}.\d{6}] .*? (\d*?) (\d*)?', sys_input)
     if regex_check:
         """ if the input matches the pattern, extract the status code
         and the file size """
         code, size = regex_check.group(2, 3)
-        file_size += int(size)
-        code = int(code)
-        if code in status_codes:
-            status_codes[code] += 1
-        else:
-            status_codes[code] = 1
+        try:
+            code = int(code)  # convert to int
+            converted_code = True
+            if code in valid_codes:
+                valid_code = True
+                try:
+                    file_size += int(size)
+                    converted = True
+                    if converted and converted_code:
+                        if code in status_codes:
+                            status_codes[code] += 1
+                        else:
+                            status_codes[code] = 1
+                        valid_code = False
+                except TypeError:
+                    continue
+            else:
+                continue
+        except ValueError:
+            continue
     if lines_count == 10:
-        print("File size: {}".format(file_size))
-        for key, value in sorted(status_codes.items()):
-            print("{}: {}".format(key, value))
+        print_result(status_codes)
         lines_count = 0
+
+    if lines_count != 10 and i == input_size - 1:
+        print_result(status_codes)
+
     elif not regex_check:
         continue
