96db5a5dd501251f7da8b8861d2e76a06ea2724d
kingHorey <Oreoluwaakinbo.oa@gmail.com>
2024-06-28 13:36:03 +0100

    add conditional to check if loop index matches the expected number of bits

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -24,7 +24,7 @@ def validUTF8(data):
 
         # index += 1
         for _ in range(1, count):
-            if data[_] >> 6 != 0b10:
+            if _ >= len(data) or data[_] >> 6 != 0b10:
                 return False
         index += count
     return True
