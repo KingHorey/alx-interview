930b904cafcdf55f578e63b52035e688e3580651
kingHorey <Oreoluwaakinbo.oa@gmail.com>
2024-06-28 13:21:46 +0100

    use bit mask for proper validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -9,22 +9,26 @@ def validUTF8(data):
         return False
     index = 0
     while index < len(data):  # in the case of nested lists
-        if data[index] > 255 or data[index] < 0:
+        if data[index] < 0:
             return False
         if data[index] >> 7 == 0b0:
-            # count = 1
             return True
-        elif data[index] >> 5 == 0b110:
+        elif data[index] & 0b11 == 0b110:
             count = 2
-        elif data[index] >> 4 == 0b1110:
+        elif data[index] & 0b111 == 0b111:
             count = 3
-        elif data[index] >> 3 == 0b11110:
+        elif data[index] & 0b1111 == 0b11110:
             count = 4
         else:
             return False
 
+        # index += 1
         for _ in range(1, count):
-            if data[index + _] >> 6 != 0b10:
+            if data[_] >> 6 != 0b10:
                 return False
         index += count
     return True
+
+
+# data = [467, 133, 108]
+# print(validUTF8(data))
