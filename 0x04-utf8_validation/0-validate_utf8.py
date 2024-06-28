dc836f018e4fa2d115952bacab2864ffd79f12e2
kingHorey <Oreoluwaakinbo.oa@gmail.com>
2024-06-28 03:41:28 +0100

    reviewed logic for utf8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,41 +1,26 @@
-#!/usr/bin/python3
-""" import annotation assistants """
-from typing import List
+#!/usr/bin/python
 
 
-def validUTF8(data: List) -> bool:
-	""" validate utf8 """
-	if not isinstance(data, list):
-		return False
-	if not data:
-		return False
-
-	count = 0
-	index = 0
-
-	#  get the number of bytes for the first index
-	if data[index] >> 7 == 0b0 or len(data) == 1:
-		return True
-	elif data[index] >> 3 == 0b1111:
-		count = 4
-	elif data[index] >> 4 == 0b1110:
-		count = 3
-	elif data[index] >> 5 == 0b110:
-		count = 2
-	elif data[index] >> 7 == 0b1:
-		count = 1
-
-	return check_sequence(count, data)
-
-
-def check_sequence(count: int, dt: List) -> bool:
-	""" check if sequence is valid """
-	index = 1
-	try:
-		for i in range(count):
-			if dt[index + i] >> 6 == 0b10:
-				continue
-			else:
-				return False
-	except IndexError:
-		pass
+def validUTF8(data):
+    if not isinstance(data, list):
+        return False
+    index = 0
+    count = 0
+    while index < len(data):  # in the case of nested lists
+        if data[index] >> 7 == 0b0:
+            count = 1
+        elif data[index] >> 5 == 0b110:
+            count = 2
+        elif data[index] >> 4 == 0b1110:
+            count = 3
+        elif data[index] >> 3 == 0b11110:
+            count = 4
+        else:
+            return False
+        index += 1
+        for _ in range(index, count):
+            if data[_] >> 6 == 0b10:
+                pass
+            else:
+                return False
+    return True
